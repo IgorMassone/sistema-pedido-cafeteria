493e4eec296b1f818952ff39a96e811281d679ca
IgorMassone <igormassone06@gmail.com>
2026-04-18 20:40:13 -0300

    feat: adiciona funções receber_dados e calcular_preco

--- cafeteria.py
+++ cafeteria.py
@@ -9,6 +9,23 @@ def receber_dados():
 
 precos = {
     "cafe": {"P": 4.00, "M": 5.50, "G": 7.00},
-    "capuccino": {"P": 5.00, "M": 7.50, "G": 9.00},
+    "capuccino": {"P": 6.00, "M": 7.50, "G": 9.00},
     "chocolate": {"P": 5.50, "M": 7.00, "G": 8.50}
 }
+
+def calcular_preco(tipoBebida, tamanhoBebida, quantidade, verificaAluno):
+    preco_unitario = precos[tipoBebida][tamanhoBebida]
+    preco_total = preco_unitario * quantidade
+    desconto = 0
+
+    if verificaAluno == "sim" and preco_total >= 15:
+        desconto = preco_total * 0.10
+        preco_total = preco_total - desconto
+
+    if preco_total >= 20:
+        print("Produto com brinde")
+    
+    return preco_total, desconto
+
+
+nome, tipoBebida, tamanhoBebida, quantidade, verificaAluno = receber_dados()
